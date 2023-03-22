84a6764e1bda9fd7a3f656acd577c02eedfab952
Oscar Mora <omora6@outlook.com>
2023-03-22 18:01:50 -0500

    Add code solution to the two sum problem

---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+int[] numbers = { 3, 3 };
+int targert = 6;
+
+Solution solution = new Solution();
+
+int[] result = solution.TwoSum(numbers, targert);
+
+Console.WriteLine("End");
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            map[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
